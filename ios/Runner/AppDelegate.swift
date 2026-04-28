19774c3e39bf67390e42ad3563ba43f2bdf49b74
HI <qwnn96386@gmail.com>
2026-04-29 07:55:54 +0800

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,12 +8,6 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    
-    // 完整保留：如果未來要使用本地通知，這行是必備的
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
