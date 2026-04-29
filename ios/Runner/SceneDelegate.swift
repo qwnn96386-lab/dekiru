d6c65a62ca45eae13b78ef3be543f2f1cb5da316
HI <qwnn96386@gmail.com>
2026-04-29 08:12:17 +0800

    Update SceneDelegate.swift

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,6 +1,7 @@
-import Flutter
 import UIKit
+import Flutter
 
-class SceneDelegate: FlutterSceneDelegate {
-
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    // 這裡留空即可，目的是讓 Xcode 找到檔案且不報錯
 }
